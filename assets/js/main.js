87c1c33ce81c843419fa03a31b5e368e2541b7b2
GB <voducthang16@gmail.com>
2021-06-06 21:35:21 +0700

    update performance
    06.06.21
    9.35pm

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
-const controls = document.querySelectorAll('.control__icon');
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const controls = $$('.control__icon');
 controls.forEach(control => {
     control.onclick = () => {
         const panel = control.nextElementSibling;
@@ -16,9 +19,9 @@ controls.forEach(control => {
     }
 });
 
-const searchIcon = document.querySelector('.fa-search');
-const cartIcon = document.querySelector('.fa-cart-plus');
-const userIcon = document.querySelector('.fa-user');
+const searchIcon = $('.fa-search');
+const cartIcon = $('.fa-cart-plus');
+const userIcon = $('.fa-user');
 
 document.addEventListener('click', e => {
     let targetElement = e.target;
@@ -54,37 +57,34 @@ document.addEventListener('click', e => {
         userIcon.nextElementSibling.classList.remove('active');
     }
 });
-
-
 // FAQ
-const faqQuestions = document.querySelectorAll('.faq__question');
-const faqAnswers = document.querySelectorAll('.faq__answer');
+function removeClass() {
+    $('.faq__question.active').classList.remove('active');
+    $('.faq__answer.active').style.maxHeight = 0;
+    $('.faq__answer.active').classList.remove('active');
+}
+const faqQuestions = $$('.faq__question');
+const faqAnswers = $$('.faq__answer');
 
 faqQuestions.forEach(question => {
     question.nextElementSibling.style.maxHeight = 0;
-    question.addEventListener('click', () => {
+    question.addEventListener('click', e => {
         const panel = question.nextElementSibling;
         height = panel.scrollHeight;
 
-        if (question.classList.contains('active')) {
-            question.classList.remove('active');
-            panel.classList.remove('active');
-            panel.style.maxHeight = 0;
-        } else {
-            question.classList.add('active');
-            panel.classList.add('active');
-            panel.style.maxHeight = `${height + 32}px`;
+        if (!$('.faq__question.active') === false) {
+            if ($('.faq__question.active') == e.target) {
+                removeClass();
+                return;
+            };
+            removeClass();
         }
 
-        faqQuestions.forEach(faqQuestion => {
-            if (faqQuestion !== question && faqQuestion.classList.contains('active')) {
-                faqQuestion.classList.remove('active');
-                faqQuestion.nextElementSibling.classList.remove('active');
-                faqQuestion.nextElementSibling.style.maxHeight = 0;
-            }
-        })
+        question.classList.add('active');
+        panel.classList.add('active');
+        panel.style.maxHeight = `${height + 32}px`;
     })
-})
+});
 
 document.addEventListener('click', e => {
     const target = e.target;
